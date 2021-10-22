933eda7a5e3bc9e316b99421909287acb576d92e
Tauhoo <tauhoo_ice@hotmail.com>
2021-10-22 16:23:52 +0700

    remve error file

--- web-interface/src/common/TwitterShare.tsx
+++
@@ -1,6 +0,0 @@
-import { useState, useEffect } from "react";
-
-const DownloadButton = () => {
-    return <a href={`https://twitter.com/intent/tweet?text=${text}`} target="_blank">Share on Twitter</a>
-}
-export default DownloadButton
